f9c5436f6fc1cb5de8c8a66bc59f856b45bbcfb2
Thai Hoang <110578242+ThaiHoang-newbie@users.noreply.github.com>
2023-06-01 11:14:12 +0700

    Add files via upload

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,21 +4,54 @@ namespace App\Http\Controllers;
 
 use App\Models\slide;
 use App\Models\products;
+use App\Models\type_products;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function getIndex()
     {
+        // Slide
         $slide = Slide::all();
-        $new_product = Products::where('new', 1)-> get();
-        return view('Page', compact('slide', 'new_product'));
+
+        // All pro
+        $product = Products::all();
+
+        //New pro
+        $new_product = Products::where('new', 1)->get();
+
+        // Type pro
+        $type = type_products::all();
+
+
+        return view('trangchu', compact('slide', 'new_product', 'type', 'product'));
     }
-    
+
     public function getID(Request $request)
     {
         $sanpham = Products::where('id', $request->id)->first();
         return view('ProDetail', compact('sanpham'));
     }
-}
 
+
+    // View Product type
+    public function getTypeProducts($type)
+    {
+        // All type products
+        $type_pro = type_products::all();
+
+
+        // A type products
+        $typeID = type_products::where('id', $type)->first();
+
+
+        // Get all products in that type product
+        $product = Products::where('id_type', $type)->get();
+
+
+        // Else
+        $product_else = Products::where('id_type', '<>', $type)->get();
+
+        return view('Product_type', compact('type_pro', 'typeID', 'product', 'product_else'));
+    }
+}
