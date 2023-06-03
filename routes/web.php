d59e8f818d24cdb4bddaed3c69748628749ee5c8
Thai Hoang <110578242+ThaiHoang-newbie@users.noreply.github.com>
2023-06-03 10:22:51 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,7 @@ Route::get('cr', [innertext::class, 'Migration']);
 
 
 
-Route::get('/page',[PageController::class, 'getIndex']);
+
 
 Route::get('/page',[PageController::class, 'getIndex']);
 
@@ -59,6 +59,22 @@ Route::get('/detail/{id}',[PageController::class, 'getID']);
 
 Route::get('/type/{id}',[PageController::class, 'getTypeProducts']);
 
+
+
+
+Route::get('/admin', [PageController::class,'getIndexAdmin']);
+
+Route::get('/admin-add-form',[PageController::class,'getAdminAdd'])->name('add-product');
+
+Route::post('/admin-add-form',[PageController::class,'postAdminAdd']);
+
+Route::get('/admin-edit-form/{id}', [PageController::class, 'getAdminEdit']);
+
+Route::post('/admin-edit',[PageController::class,'postAdminEdit']);
+
+Route::post('/admin-delete/{id}',[PageController::class,'postAdminDelete']);
+
+Route::get('admin-export',[PageController::class,'exportAdminProduct'])->name('export');
 
 
 
